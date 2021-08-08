bc86aaa39ea56f8143d4f3e1f02923bab5f4a5cb
Madu97 <2018is050@stu.ucsc.cmb.ac.lk>
2021-08-08 18:50:20 +0530

    commit

--- furnicom/src/Component/supplier-dashboard/SupplierProducts.js
+++ furnicom/src/Component/supplier-dashboard/SupplierProducts.js
@@ -25,15 +25,6 @@ const SupplierProducts = (userData) => {
     return (
         <div className="bg-theme-pale d-block  justify-content-center pb-3 p-3">
             
-            <div className="d-flex justify-content-left">
-                <button className="btn btn-lg btn-warning fc-white d-flex justify-content-center align-items-center">Add Items
-            <div>
-                <i class="fa fa-plus-circle m-1 fa-1x fc-white" ></i>
-                </div></button>
-            </div>
-            <br></br>
-
-            
             <div className="row justify-content-center fc-white">
             {Productdata.map((person) =>
                 <div className="col-md-4 col-sm-7 d-block text-center">
@@ -41,7 +32,6 @@ const SupplierProducts = (userData) => {
                     <div className="m-2">
                         <div class="fc-darkblue font-weight-bold">{person.name}</div>
                         <div class="fc-theme ">Rs.{person.price}</div>
-                        <button className="btn btn-info"><i class="fa fa-edit fc-white mr-1" ></i>Edit</button>
                         <button className="btn btn-success mx-1">More Details</button>
                     </div>
                 </div> 
