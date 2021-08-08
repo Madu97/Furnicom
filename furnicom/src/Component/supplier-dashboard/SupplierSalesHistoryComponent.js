cc03a44d600246ccaad616ebda4d8db21dc0751a
Madu97 <2018is050@stu.ucsc.cmb.ac.lk>
2021-08-08 18:58:33 +0530

    Supplier sales

--- furnicom/src/Component/supplier-dashboard/SupplierSalesHistoryComponent.js
+++ furnicom/src/Component/supplier-dashboard/SupplierSalesHistoryComponent.js
@@ -24,33 +24,30 @@ const SupplierSales = (userData) => {
         return (
             <div className="col-12  d-block pl-1 pr-1 ">
             <div className="d-flex justify-content-center table-responsive bg-order-his-color rounded">
-            <table class="table fc-white text-left ">
-                <thead className="fs-30 text-left">
+            <table class="table fc-white text-center ">
+                <thead className="fs-30 text-center">
                     <tr>
                         <th scope="col">Order No</th>
                         <th scope="col">Item</th>
-                        <th scope="col">Quantity</th>
+                        <th scope="col">Item Name</th>
                         <th scope="col">View</th>
                     </tr>
-                    </thead>
+                 </thead>
                     <tbody className="align-items-center">
 
                     {Salesdata.map((person) =>
-
-
                     <tr>
                     <th scope="row">{person.order_id}</th>
                     <td>
-                    <div className="d-flex justify-content-start align-items-center">
+                    <div className="d-flex justify-content-center align-items-center">
                      <img src={person.thumb} width="100px" height="100px" className="m-2 rounded"></img>
-                     <p className="fs-22">{person.name}</p>
                     </div>
                     </td>
-                    <td className=" align-middle">{person.quantity}</td>
+                    <td className=" align-middle">{person.name}</td>
                     <td className=" align-middle"> <button className="btn btn-warning fc-white font-weight-bold">More Details</button></td>
                     </tr>
                     )}
-                      </tbody>
+                    </tbody>
 
                 </table>
                 </div>
